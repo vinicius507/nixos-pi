27662b69c58ccc15115059312eed8d121fa2085f
Vinicius Oliveira <vinicius@myriad.codes>
2023-11-24 14:23:40 -0300

    fix: updates openssh authorized keys to new ssh key

--- hosts/shared/users.nix
+++ hosts/shared/users.nix
@@ -7,7 +7,7 @@
       "wheel"
     ];
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAhSMRMlnqNZ8VlxE+fGiCkiGpl7X6etvtjBtkZvZfnP"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGPWdo2yzi5UwwXnc96HOhSaZ/d2k8bvlXFP8msKGdhW"
     ];
   };
 }
