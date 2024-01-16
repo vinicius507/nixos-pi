4ace3273ab2caf142b0f1cfb199fbedbfa8ae679
Vinicius Oliveira <vinicius@myriad.codes>
2024-01-16 18:26:36 -0300

    feat: adds step-ca files to secrets

--- hosts/pi/services/step-ca.nix
+++ hosts/pi/services/step-ca.nix
@@ -1,14 +1,17 @@
-{config, ...}: {
+{config, ...}: let
+  inherit (config.sops) secrets;
+in {
+  users.users.step-ca.extraGroups = ["www"];
   services.step-ca = {
     enable = true;
     address = "127.0.0.1";
     port = 8000;
-    intermediatePasswordFile = config.sops.secrets."step-ca/intermediatePassword".path;
+    intermediatePasswordFile = secrets."services/step-ca/intermediate-password".path;
     settings = {
-      dnsNames = ["localhost"];
-      root = "/var/lib/step-ca/certs/root_ca.crt";
-      crt = "/var/lib/step-ca/certs/intermediate_ca.crt";
-      key = config.sops.secrets."step-ca/intermediateCaKey".path;
+      dnsNames = ["ca.dezano.io" "localhost"];
+      root = secrets."services/step-ca/root-cert".path;
+      crt = secrets."services/step-ca/intermediate-cert".path;
+      key = secrets."services/step-ca/intermediate-key".path;
       db = {
         type = "badger";
         dataSource = "/var/lib/step-ca/db";
@@ -32,12 +35,27 @@
       };
     };
   };
-  sops.secrets."step-ca/intermediateCaKey" = {
-    owner = config.users.users.step-ca.name;
-    group = config.users.users.step-ca.group;
-  };
-  sops.secrets."step-ca/intermediatePassword" = {
-    owner = config.users.users.step-ca.name;
-    group = config.users.users.step-ca.group;
+  sops.secrets = {
+    "services/step-ca/root-cert" = {
+      owner = config.users.users.step-ca.name;
+      group = config.users.groups.www.name;
+      mode = "0440";
+      restartUnits = ["step-ca.service" "traefik.service"];
+    };
+    "services/step-ca/intermediate-cert" = {
+      owner = config.users.users.step-ca.name;
+      group = config.users.users.step-ca.group;
+      restartUnits = ["step-ca.service"];
+    };
+    "services/step-ca/intermediate-key" = {
+      owner = config.users.users.step-ca.name;
+      group = config.users.users.step-ca.group;
+      restartUnits = ["step-ca.service"];
+    };
+    "services/step-ca/intermediate-password" = {
+      owner = config.users.users.step-ca.name;
+      group = config.users.users.step-ca.group;
+      restartUnits = ["step-ca.service"];
+    };
   };
 }
