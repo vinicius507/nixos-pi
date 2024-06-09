6ed2ada1c0af1925fe3dcd1ea8b31f0991b2fa58
Vinicius Oliveira <viniciusp.olivera@gmail.com>
2024-06-09 01:14:57 -0300

    feat(psql): Adds host entry for tailscale network

--- hosts/pi/services/postgres.nix
+++ hosts/pi/services/postgres.nix
@@ -15,6 +15,7 @@
       local all all trust
       host all all 127.0.0.1/32 trust
       host all all ::1/128 trust
+      host all all 100.64.0.0/10 trust
     '';
     identMap = ''
       superuser_map      root      postgres
