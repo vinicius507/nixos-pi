b4f51f2122f1eef8f683ab1b7fa83655db2a1ed9
Vinicius Oliveira <viniciusp.olivera@gmail.com>
2024-08-29 00:22:07 -0300

    fix: Adds firewall rule to whitelist docker interface

--- hosts/shared/networking.nix
+++ hosts/shared/networking.nix
@@ -1,6 +1,11 @@
 {
   networking = {
-    firewall.enable = true;
+    firewall = {
+      enable = true;
+      extraCommands = ''
+        iptables -A INPUT -p tcp --destination-port 8000 -s 172.17.0.1/16 -j ACCEPT
+      '';
+    };
     nameservers = ["127.0.0.1"];
     networkmanager.enable = true;
   };
